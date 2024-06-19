b2ebd8a86d057decd6ffce0aedee52ee0e57e31c
Vinod V <vinod@tunerlabs.com>
2024-06-19 12:46:05 +0530

    Styling changes and package upgradation done for project-details page

--- src/app/project/project-details.page.ts
+++ src/app/project/project-details.page.ts
@@ -5,6 +5,7 @@ import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-project',
   templateUrl: './project-details.page.html',
+  styleUrls: ['./project-details.page.scss']
 })
 export class ProjectDetailsPage  implements OnInit {
   router: Router;
@@ -12,18 +13,25 @@ export class ProjectDetailsPage  implements OnInit {
   config = {
     maxFileSize: 50,
     baseUrl: window['env' as any]['baseURL' as any],
-    accessToken: localStorage.getItem('accToken')
+    accessToken: localStorage.getItem('accToken'),
+    profileInfo: {
+      "entityType" : "block",
+      "entityTypeId" : "5f32d8228e0dc8312404056e",
+      "entities" : [
+          "5fd1b52ab53a6416aaeefc80",
+          "5fd098e2e049735a86b748ac",
+          "5fd1b52ab53a6416aaeefc83",
+          "5fd1b52ab53a6416aaeefb20"
+      ],
+      "role" : "BEO,HM"
+    }
   }
     constructor(private navCtrl: NavController) {
       this.router = inject(Router);
     }
 
     ngOnInit(): void {
-      this.router.events.subscribe(event => {
-        if (event instanceof NavigationEnd) {
-          this.projectData = this.router.getCurrentNavigation()?.extras.state;
-        }
-      });
+      this.projectData = this.router.getCurrentNavigation()?.extras.state
     }
 
     goBack(){
