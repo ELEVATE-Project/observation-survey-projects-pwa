f256cc5d0a8e5c74091cfbacfcc0a75e9e5c25f7
Syed Ameen <syedameenabrar786@gmail.com>
2024-07-12 01:23:16 +0530

    profile image

--- src/app/profile-edit/profile-edit.page.ts
+++ src/app/profile-edit/profile-edit.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, HostListener, destroyPlatform } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { LoaderService } from '../services/loader/loader.service';
 import { finalize, catchError, map } from 'rxjs/operators';
 import { forkJoin, throwError } from 'rxjs';
@@ -7,9 +7,8 @@ import { ToastService } from '../services/toast/toast.service';
 import urlConfig from 'src/app/config/url.config.json';
 import { FETCH_Profile_FORM } from '../core/constants/formConstant';
 import { MainFormComponent } from 'elevate-dynamic-form';
-import { AlertController, NavController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 import { AttachmentService } from '../services/attachment/attachment.service';
-
 @Component({
   selector: 'app-profile-edit',
   templateUrl: './profile-edit.page.html',
@@ -20,7 +19,7 @@ export class ProfileEditPage {
   formJson: any = [];
   urlProfilePath = urlConfig['profileListing'];
   formData: any;
-  localImage:any;
+  localImage: any;
 
   constructor(
     private apiBaseService: ApiBaseService,
@@ -38,40 +37,37 @@ export class ProfileEditPage {
       formJson: this.apiBaseService.post(urlConfig['formListing'].listingUrl, FETCH_Profile_FORM),
       profileFormData: this.apiBaseService.get(urlConfig['profileListing'].listingUrl)
     })
-    .pipe(
-      finalize(async () => await this.loader.dismissLoading()),
-      catchError(err => {
-        this.toastService.presentToast(err?.error?.message, 'danger');
-        return throwError(() => err);
-      })
-    )
-    .subscribe((res: any) => {
-      if (res.formJson?.status === 200 && res.profileFormData?.status === 200) {
-        this.formJson = res.formJson?.result?.data;
-        this.formData = res.profileFormData?.result;
-    this.formJson.isUploaded = true;
-    this.getDownloadImage();
-        this.mapProfileDataToFormJson(this.formData);
-        this.formJson.map((control: any) => {
-          if (control.dynamicEntity) {
-            this.getOptionsData(control.name);
-          }
-        });
+      .pipe(
+        finalize(async () => await this.loader.dismissLoading()),
+        catchError(err => {
+          this.toastService.presentToast(err?.error?.message, 'danger');
+          return throwError(() => err);
+        })
+      )
+      .subscribe((res: any) => {
+        if (res.formJson?.status === 200 && res.profileFormData?.status === 200) {
+          this.formJson = res.formJson?.result?.data;
+          this.formData = res.profileFormData?.result;
+          this.formJson.isUploaded = true;
+          this.mapProfileDataToFormJson(this.formData);
+          this.formJson.map((control: any) => {
+            if (control.dynamicEntity) {
+              this.getOptionsData(control.name);
+            }
+          });
+        }
       }
-    }
-  );
+      );
   }
 
   mapProfileDataToFormJson(formData?: any) {
-      Object.entries(formData).forEach(([key, value]:any) => {
-        const control = this.formJson.find((control: any) => control.name === key);
-        if (control) {
-            control.value =  typeof(value) === 'string' ? String(value):value;
-            control.value =  typeof(value) === 'object' ? value?.value:value?.value;
-            console.log(value)
-
-        }
-      });
+    this.getDownloadImage();
+    Object.entries(formData).forEach(([key, value]: any) => {
+      const control = this.formJson.find((control: any) => control.name === key);
+      if (control) {
+        control.value = typeof (value) === 'string' ? String(value) : value?.value;
+      }
+    });
   }
 
   getOptionsData(entityType: string, entityId?: string) {
@@ -88,10 +84,10 @@ export class ProfileEditPage {
           return throwError(() => err);
         })
       )
-      .subscribe((res:any) => {
+      .subscribe((res: any) => {
         if (res?.status === 200) {
           const result = control.dynamicEntity ? res?.result : res?.result?.data;
-          if(result){
+          if (result) {
             const options = result.map((entity: any) => ({
               label: entity.name,
               value: entity._id
@@ -154,28 +150,28 @@ export class ProfileEditPage {
 
   updateProfile() {
     if (this.formLib?.myForm.valid) {
-
       if (this.formJson.image && !this.formJson.isUploaded) {
         this.getImageUploadUrl(this.localImage);
       } else {
         this.formJson.isUploaded = false;
-        let abc = this.formLib?.myForm.value;
-        abc.location = "a";
-        abc.about = "q";
-        this.apiBaseService.patch(this.urlProfilePath.updateUrl, abc)
-        .pipe(
-          catchError(err => {
-            this.toastService.presentToast(err?.error?.message, 'danger');
-            return throwError(() => err);
-          })
-        )
-        .subscribe((res:any) => {
-          if (res?.status === 200) {
-            this.toastService.presentToast(res?.message || 'Profile Updated Sucessfully', 'success');
-          }else{
-            this.toastService.presentToast(res?.message, 'warning');
-          }
-        });
+        let payload = this.formLib?.myForm.value;
+        payload.location = "bangalore";
+        payload.about = "PWA";
+        payload.image = this.formJson?.image;
+        this.apiBaseService.patch(this.urlProfilePath.updateUrl, payload)
+          .pipe(
+            catchError(err => {
+              this.toastService.presentToast(err?.error?.message, 'danger');
+              return throwError(() => err);
+            })
+          )
+          .subscribe((res: any) => {
+            if (res?.status === 200) {
+              this.toastService.presentToast(res?.message || 'Profile Updated Sucessfully', 'success');
+            } else {
+              this.toastService.presentToast(res?.message, 'warning');
+            }
+          });
       }
     } else {
       this.formLib?.myForm.markAllAsTouched();
@@ -183,7 +179,7 @@ export class ProfileEditPage {
     }
   }
 
-   handleSelectFocus(controlName: any) {
+  handleSelectFocus(controlName: any) {
     const control = this.formJson.find((ctrl: any) => ctrl.name === controlName);
     if (control && control.dependsOn) {
       const dependentControl = this.formLib?.myForm.get(control.dependsOn);
@@ -197,7 +193,7 @@ export class ProfileEditPage {
     this.navCtrl.back();
   }
 
-  async imageUploadEvent(event:any) {
+  async imageUploadEvent(event: any) {
     this.localImage = event.target.files[0];
     var reader = new FileReader();
     reader.readAsDataURL(event.target.files[0]);
@@ -207,51 +203,49 @@ export class ProfileEditPage {
     }
   }
 
-  imageRemoveEvent(event:any){
+  imageRemoveEvent(event: any) {
     this.formJson.image = '';
     this.formLib?.myForm.markAsDirty();
     this.formJson.isUploaded = true;
   }
 
-  async getImageUploadUrl(file:any) {
+  async getImageUploadUrl(file: any) {
     this.loader.showLoading("Please wait while uploading...");
-    // this.apiBaseService.get(this.urlProfilePath.getSessionImageUploadUrl + file.name.replace(/[^A-Z0-9]+/ig, "_").toLowerCase())
     this.apiBaseService.get(this.urlProfilePath.getSessionImageUploadUrl + file?.name.replace(/[^A-Z0-9]+/ig, "_").toLowerCase())
-
-    .pipe(
-      finalize(async () => await this.loader.dismissLoading()),
-      catchError(err => {
-        this.toastService.presentToast(err?.error?.message, 'danger');
-        return throwError(() => err);
-      })
-    )
-    .subscribe((res:any) => {
-    return this.upload(file, res.result).subscribe()
-    });
+      .pipe(
+        finalize(async () => await this.loader.dismissLoading()),
+        catchError(err => {
+          this.toastService.presentToast(err?.error?.message, 'danger');
+          return throwError(() => err);
+        })
+      )
+      .subscribe((res: any) => {
+        return this.upload(file, res.result).subscribe()
+      });
   }
 
 
-   getDownloadImage() {
-    this.apiBaseService.get(this.urlProfilePath.getDownloadableUrl + 'users/162-1720684914372-image_removebg_preview_2_png')
-    .pipe(
-      finalize(async () => await this.loader.dismissLoading()),
-      catchError(err => {
-        this.toastService.presentToast(err?.error?.message, 'danger');
-        return throwError(() => err);
-      })
-    )
-    .subscribe((res:any) => {
-      this.formJson.image = res.result
-      this.formJson.isUploaded = false;
-    });
+  getDownloadImage() {
+    this.apiBaseService.get(this.urlProfilePath.getDownloadableUrl + this.formData?.image)
+      .pipe(
+        finalize(async () => await this.loader.dismissLoading()),
+        catchError(err => {
+          this.toastService.presentToast(err?.error?.message, 'danger');
+          return throwError(() => err);
+        })
+      )
+      .subscribe((res: any) => {
+        this.formJson.image = res.result
+        this.formJson.isUploaded = false;
+      });
   }
 
-  upload(data:any, uploadUrl:any) {
-    return this.attachment.cloudImageUpload(data,uploadUrl).pipe(
-      map((()=>{
-      this.formJson.image = uploadUrl.destFilePath;
-      this.formJson.isUploaded = true;
-      this.updateProfile();
-    })))
+  upload(data: any, uploadUrl: any) {
+    return this.attachment.cloudImageUpload(data, uploadUrl).pipe(
+      map((() => {
+        this.formJson.image = uploadUrl.destFilePath;
+        this.formJson.isUploaded = true;
+        this.updateProfile();
+      })))
   }
 }
