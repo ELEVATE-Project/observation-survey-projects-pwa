98eaa865ef17feafd20c76db90496a988a4faae4
Syed Ameen <syedameenabrar786@gmail.com>
2024-08-07 14:27:27 +0530

    optimized code profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { ProfileService } from '../services/profile/profile.service';
 import { NavController } from '@ionic/angular';
 import { LoaderService } from '../services/loader/loader.service';
@@ -12,12 +12,12 @@ import { ToastService } from '../services/toast/toast.service';
 export class ProfilePage {
   formJson: any = [];
   formData: any;
-  
+
   constructor(private profileService: ProfileService,
     private navCtrl: NavController,
     private loader: LoaderService,
     private toastService: ToastService
-  ){}
+  ) { }
 
   ionViewWillEnter() {
     this.loadFormAndData();
@@ -26,22 +26,22 @@ export class ProfilePage {
   async loadFormAndData() {
     await this.loader.showLoading("Please wait while loading...");
     this.profileService.getFormJsonAndData()
-    .pipe(
-      catchError((err) => {
-        this.toastService.presentToast(err?.error?.message || 'Error loading profile data. Please try again later.', 'danger');
-        throw err;
-      }),
-      finalize(async () => await this.loader.dismissLoading())
-    )
-    .subscribe(([formJsonRes, profileFormDataRes]: any) => {
-      if(formJsonRes?.status === 200 || profileFormDataRes?.status === 200){
-        this.formJson = formJsonRes?.result?.data;
-        this.formData = profileFormDataRes?.result;
-        this.mapProfileDataToFormJson(this.formData);
-      }else{
-        this.toastService.presentToast('Failed to load profile data. Please try again later.', 'danger');
-      }
-    });
+      .pipe(
+        catchError((err) => {
+          this.toastService.presentToast(err?.error?.message || 'Error loading profile data. Please try again later.', 'danger');
+          throw err;
+        }),
+        finalize(async () => await this.loader.dismissLoading())
+      )
+      .subscribe(([formJsonRes, profileFormDataRes]: any) => {
+        if (formJsonRes?.status === 200 || profileFormDataRes?.status === 200) {
+          this.formJson = formJsonRes?.result?.data;
+          this.formData = profileFormDataRes?.result;
+          this.mapProfileDataToFormJson(this.formData);
+        } else {
+          this.toastService.presentToast('Failed to load profile data. Please try again later.', 'danger');
+        }
+      });
   }
 
   mapProfileDataToFormJson(formData?: any) {
@@ -53,23 +53,26 @@ export class ProfilePage {
         value: role.title
       }));
     }
-    if (!this.formJson.find((control: any) => control.name === 'user_roles')) {
-      this.formJson.push({ name: 'user_roles', type:'text' });
+    this.formJson.push({ name: 'user_roles', type: 'chip' });
+    const rolesIndex = this.formJson.findIndex((control: any) => control.name === 'roles');
+    if (rolesIndex !== -1) {
+      this.formJson.splice(rolesIndex, 1);
     }
-    Object.entries(formData).map(([key, value]: any) => {
+
+    Object.entries(formData).forEach(([key, value]: any) => {
       const control = this.formJson.find((control: any) => control.name === key);
       if (control) {
         control.disabled = true;
         if (control.type === 'select' || control.type === 'chip') {
           control.type = 'text';
-        } else {
-          control.type = control.type;
         }
-        if(Array.isArray(value)) {
-          control.value = value.map((item: any) => item.value || item);
-        } else {
-          control.value = typeof value === 'string' ? String(value) : value?.label;
-        }
+
+        control.value = Array.isArray(value)
+          ? value.map((item: any) => item.value || item)
+          : typeof value === 'string'
+            ? String(value)
+            : value?.label;
+            
         control.validators = false;
         control.label = this.capitalizeLabelFirstLetter(control.name);
       }
