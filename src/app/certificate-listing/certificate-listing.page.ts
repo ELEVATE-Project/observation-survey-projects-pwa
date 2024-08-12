c412d564a5c8e8db6d558d9b37c7838e50257a2a
naveen <chennamsettynaveen945@gmail.com>
2024-08-12 19:55:48 +0530

    "API intergartion of the certificate listing"

--- src/app/certificate-listing/certificate-listing.page.ts
+++ src/app/certificate-listing/certificate-listing.page.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,inject} from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { ApiBaseService } from '../services/base-api/api-base.service';
+import { LoaderService } from '../services/loader/loader.service';
+import { ToastService } from '../services/toast/toast.service';
+import { finalize } from 'rxjs';
+import urlConfig from 'src/app/config/url.config.json';
 
 @Component({
   selector: 'app-certificate-listing',
@@ -8,12 +13,37 @@ import { NavController } from '@ionic/angular';
 })
 export class CertificateListingPage implements OnInit {
   certificates:any;
-  constructor(private navCtrl: NavController) { }
+  toastService: ToastService;
+  baseApiService: any;
+  loader: LoaderService;
+  constructor(private navCtrl: NavController) {
+    this.baseApiService = inject(ApiBaseService);
+    this.loader = inject(LoaderService)
+    this.toastService = inject(ToastService)
+   }
 
   ngOnInit() {
+    this.getCertificateList()
   }
   goBack() {
     this.navCtrl.back();
   }
-
+  async getCertificateList() {
+    await this.loader.showLoading("Please wait while loading...");
+    this.baseApiService
+      .get(urlConfig.certificate.certificateListing)
+      .pipe(
+        finalize(async () => {
+          await this.loader.dismissLoading();
+        })
+      )
+      .subscribe((res: any) => {
+        console.log(res)
+        this.certificates=res.result.data
+      },
+        (err: any) => {
+          this.toastService.presentToast(err?.error?.message, 'danger');
+        }
+      );
+  }
 }
