910a501691f55e41f5c065c764e03b2a3bd2a5ff
naveen <chennamsettynaveen945@gmail.com>
2024-08-13 14:39:59 +0530

    Implementation of the QR scanner functionality

--- src/app/services/util/util.service.ts
+++ src/app/services/util/util.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
+import { CertificateVerificationPopoverComponent } from 'src/app/shared/certificate-verification-popover/certificate-verification-popover.component';
+import { PopoverController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilService {
 
-  constructor() { }
+  constructor(private popoverController: PopoverController) { }
 
   isMobile(){
     return /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent);
+  }
+  async openCertificateVerificationPopover(res: any) {
+    const popover = await this.popoverController.create({
+      component: CertificateVerificationPopoverComponent,
+      componentProps: {
+        data: res
+      },
+      cssClass: 'certificate-popup',
+      backdropDismiss: false
+    });
+    await popover.present();
+    await popover.onDidDismiss(); 
   }
 }
