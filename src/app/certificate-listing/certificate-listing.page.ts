7863b38402675e3b8975b8747ecdeef9e5090574
naveen <chennamsettynaveen945@gmail.com>
2024-08-13 12:08:18 +0530

    Revert ""API intergartion of the certificate listing""
    This reverts commit c412d564a5c8e8db6d558d9b37c7838e50257a2a.

--- src/app/certificate-listing/certificate-listing.page.ts
+++ src/app/certificate-listing/certificate-listing.page.ts
@@ -1,10 +1,5 @@
-import { Component, OnInit ,inject} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { ApiBaseService } from '../services/base-api/api-base.service';
-import { LoaderService } from '../services/loader/loader.service';
-import { ToastService } from '../services/toast/toast.service';
-import { finalize } from 'rxjs';
-import urlConfig from 'src/app/config/url.config.json';
 
 @Component({
   selector: 'app-certificate-listing',
@@ -13,17 +8,9 @@ import urlConfig from 'src/app/config/url.config.json';
 })
 export class CertificateListingPage implements OnInit {
   certificates:any;
-  toastService: ToastService;
-  baseApiService: any;
-  loader: LoaderService;
-  constructor(private navCtrl: NavController) {
-    this.baseApiService = inject(ApiBaseService);
-    this.loader = inject(LoaderService)
-    this.toastService = inject(ToastService)
-   }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
-    this.getCertificateList()
   }
   goBack() {
     this.navCtrl.back();
