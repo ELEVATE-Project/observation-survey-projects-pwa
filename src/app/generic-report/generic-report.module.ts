d0eb417b8f91dc126257c8245e2ee74862b74cfa
5Amogh <amoghavarsh@navadhiti.com>
2024-08-23 08:17:39 +0530

    Report enhancements

--- src/app/generic-report/generic-report.module.ts
+++ src/app/generic-report/generic-report.module.ts
@@ -5,13 +5,18 @@ import { GenericReportRoutingModule } from './generic-report-routing.module';
 import { SanitizeUrlPipe } from './pipes/sanitize-url.pipe';
 import { GenericReportComponent } from './generic-report.component';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [GenericReportComponent,SanitizeUrlPipe],
   imports: [
     CommonModule,
+    MatIconModule,
+    FormsModule,
     IonicModule,
+    
     GenericReportRoutingModule
   ]
 })
