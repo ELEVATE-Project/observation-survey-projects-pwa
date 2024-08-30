de3ebc263c87905fe9d67ca2071dc14d4c7fdce3
5Amogh <amoghavarsh@navadhiti.com>
2024-08-30 14:10:31 +0530

    Questionnaire version update, urls update for different subdomains

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseURL: 'https://dev.elevate-apis.shikshalokam.org',
   projectsBaseURL:"https://project-dev.elevate-apis.shikshalokam.org",
-  samikshaBaseURL:"https://dev.elevate-apis.shikshalokam.org"
+  samikshaBaseURL:"https://survey-dev.elevate-apis.shikshalokam.org"
 };
