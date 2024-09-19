e198676b709f81ee8cbc60a0eea934de5e8d60ef
Syed Ameen <syedameenabrar786@gmail.com>
2024-09-19 12:05:11 +0530

    page leave confirmation box in questioner player

--- src/app/questionnaire/questionnaire-routing.module.ts
+++ src/app/questionnaire/questionnaire-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+import { GuardService } from '../services/guard/guard.service';
 
 const routes: Routes = [
   {
     path:'',
-    component:QuestionnaireComponent
+    component:QuestionnaireComponent,
+    canDeactivate: [GuardService]
   }
 ];
 
