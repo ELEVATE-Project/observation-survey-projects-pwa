15146e9ff954057213dce0493c51c9dc8432a68d
Vinod V <vinod@tunerlabs.com>
2024-09-23 15:19:45 +0530

    Exit popup issue fix and title change

--- src/app/services/guard/guard.service.ts
+++ src/app/services/guard/guard.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
 export interface isDeactivatable {
@@ -11,8 +12,12 @@ export interface isDeactivatable {
 export class GuardService {
   
   canDeactivate(
-    component: isDeactivatable
+    component: isDeactivatable,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
+    if(nextState.url == "/login") return true
     return component?.canPageLeave ? component.canPageLeave() : true;
   }
 }
