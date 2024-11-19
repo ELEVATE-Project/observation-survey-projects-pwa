c9b99c5c72e8c3cac8ee884ddeb538316a9db2da
Syed Ameen <syedameenabrar786@gmail.com>
2024-11-19 19:18:14 +0530

    observation flow

--- src/assets/env/env.js
+++ src/assets/env/env.js
@@ -1,7 +1,6 @@
 window["env"] = {
     production: true,
-    baseURL: 'https://project-dev.elevate-apis.shikshalokam.org',
-    projectsBaseURL: 'https://project-dev.elevate-apis.shikshalokam.org',
-    surveyBaseURL: 'https://survey-dev.elevate-apis.shikshalokam.org',
-    capabilities: 'all'
+    baseURL: '<base-url>',
+    projectsBaseURL: '<project-base-url>',
+    surveyBaseURL: '<survey-base-url>'
 };
