18221e6e8bbcb7e5605518e6574d2c69aff79f71
farhanp1502 <farhanpasha1502@gmail.com>
2024-11-26 10:51:57 +0530

    chnages

--- src/app/profile-edit/profile-edit.page.ts
+++ src/app/profile-edit/profile-edit.page.ts
@@ -215,7 +215,7 @@ export class ProfileEditPage implements isDeactivatable {
               let entityIds = entityData?.value ;
 
               const dependentValue = this.getDependentValue(control, entityData);
-  
+        
               if(dependentValue || !this.dynamicEntityValueChanged){
                 this.getOptionsData(control.name, entityIds, this.formJson2);
                 if (firstLoad) {
