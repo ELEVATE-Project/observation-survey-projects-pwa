2eb5574a201090384434578b17566af0028a4698
5Amogh <amoghavarsh@navadhiti.com>
2024-12-05 12:49:55 +0530

    Port reverted to 1818

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 1008;
+const port = process.env.PORT || 1818;
 
 app.use(express.static(path.join(__dirname, 'www')));
 
