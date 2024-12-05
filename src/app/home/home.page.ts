15512bdfc1a588639544d5f5a45a63a6c14d78f6
5Amogh <amoghavarsh@navadhiti.com>
2024-12-05 13:32:24 +0530

    'Test log removed'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -42,7 +42,6 @@ export class HomePage {
     this.loader = inject(LoaderService)
     this.toastService = inject(ToastService)
     this.setHeaderConfig();
-    console.log('Testing Amogh for deployment')
   }
 
   ionViewWillEnter() {
