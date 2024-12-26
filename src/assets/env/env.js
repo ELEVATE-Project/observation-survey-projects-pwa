9ff82e561dddcd2bf8b49500e85f2abc28b2ec58
Syed Ameen <syedameenabrar786@gmail.com>
2024-12-26 18:24:25 +0530

    filter pdf version updated

--- src/assets/env/env.js
+++ src/assets/env/env.js
@@ -1,6 +1,15 @@
 window["env"] = {
     production: true,
-    baseURL: '<base-url>',
-    projectsBaseURL: '<project-base-url>',
-    surveyBaseURL: '<survey-base-url>'
+    baseURL: 'https://project-dev.elevate-apis.shikshalokam.org',
+    projectsBaseURL: 'https://project-dev.elevate-apis.shikshalokam.org',
+    surveyBaseURL: 'https://survey-dev.elevate-apis.shikshalokam.org',
+    capabilities: 'all'
 };
+
+// window["env"] = {
+//     production: true,
+//     baseURL: 'https://project-qa.elevate-apis.shikshalokam.org',
+//     projectsBaseURL: 'https://project-qa.elevate-apis.shikshalokam.org',
+//     surveyBaseURL: 'https://qa.elevate-apis.shikshalokam.org/',
+//     capabilities: 'all'
+// };
