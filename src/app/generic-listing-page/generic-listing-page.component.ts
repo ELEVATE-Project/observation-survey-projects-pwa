a08a591ffd42c0031d7d29a663d9e5341d12bea6
naveen <chennamsettynaveen945@gmail.com>
2025-01-27 10:53:44 +0530

    Fliker issue in Explore page.

--- src/app/generic-listing-page/generic-listing-page.component.ts
+++ src/app/generic-listing-page/generic-listing-page.component.ts
@@ -28,6 +28,7 @@ export class GenericListingPageComponent  implements OnInit {
   isMenuOpen = true
   filterQuery = ""
   noData:boolean=false;
+  searchNoData:boolean=false;
 
   searchBar = true
   isRecording: boolean = false;
@@ -152,6 +153,7 @@ export class GenericListingPageComponent  implements OnInit {
   }
 
   ionViewWillEnter() {
+    this.searchNoData=false
     this.searchBar = true;
     this.reset();
     this.noData = true;
@@ -183,6 +185,7 @@ export class GenericListingPageComponent  implements OnInit {
       next: async (response: any) => {
         await this.loaderService.dismissLoading();
         this.noData = false;
+        this.searchNoData=true
         if (response) {
           this.listingData = this.listingData.concat(
             response?.result?.data || []
@@ -216,6 +219,7 @@ export class GenericListingPageComponent  implements OnInit {
       },
       error: async (error: any) => {
         await this.loaderService.dismissLoading();
+        this.searchNoData=true
         this.noData = false;
         this.toastService.presentToast(error.error.message, 'danger');
         this.detectChangesPostVoiceSearch();
