c07c0fbf254a279a026178d8b506e7a3a8c96fb9
Vinod V <vinod@tunerlabs.com>
2025-04-03 14:57:16 +0530

    Port changes done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8181;
+const port = process.env.PORT || 8182;
 
 app.use(express.static(path.join(__dirname, 'www')));
 
