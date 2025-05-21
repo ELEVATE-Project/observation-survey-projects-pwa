0bd82eb7db75212950def3e68d741fe830ec87f3
farhanp1502 <farhanpasha1502@gmail.com>
2025-05-21 20:41:08 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,14 +27,13 @@ import { SharedModule } from './shared/shared.module';
 import { FormsService } from 'formstore-cache';
 import { PrivacyPolicyPopupComponent } from './shared/privacy-policy-popup/privacy-policy-popup.component';
 import { FormsModule } from '@angular/forms';
-import { PopUpComponent } from './shared/pop-up/pop-up.component';
 
 export function translateHttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
 }
 
 @NgModule({
-  declarations: [AppComponent,CertificateVerificationPopoverComponent,ShareLinkPopupComponent,ShortUrlPipe,RedirectionHandlerComponent,PageNotFoundComponent,PrivacyPolicyPopupComponent,PopUpComponent],
+  declarations: [AppComponent,CertificateVerificationPopoverComponent,ShareLinkPopupComponent,ShortUrlPipe,RedirectionHandlerComponent,PageNotFoundComponent,PrivacyPolicyPopupComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,FormsModule,
     SlAuthLibModule, BrowserAnimationsModule,SharedModule,
     TranslateModule.forRoot({
