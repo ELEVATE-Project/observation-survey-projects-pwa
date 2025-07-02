7f0065249dc07a84d15bd97aba83c1c810db6972
farhanp1502 <farhanpasha1502@gmail.com>
2025-07-02 17:43:13 +0530

    reverted the space

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 8181;
 app.use('/ml', express.static(path.join(__dirname, 'www')));
 
 app.get('/ml/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'www', 'index.html'));
-});
+    res.sendFile(path.join(__dirname, 'www', 'index.html'));
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
