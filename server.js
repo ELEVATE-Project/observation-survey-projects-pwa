e48fe6cb6e5a8dc9ac67aa5482cc957bedbef6bd
farhanp1502 <farhanpasha1502@gmail.com>
2025-07-02 17:36:37 +0530

    server.js file changes for the basehref

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8181;
 
-app.use(express.static(path.join(__dirname, 'www')));
+app.use('/ml', express.static(path.join(__dirname, 'www')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'www', 'index.html'));
-  });
+app.get('/ml/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'www', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
