94910ed952f7877775566ab4ddfd0f7bf48b3f9d
Vinod V <vinod@tunerlabs.com>
2026-03-18 19:47:04 +0530

    Change in logic for profile complete percentage

--- src/app/location-update/location-update.page.ts
+++ src/app/location-update/location-update.page.ts
@@ -302,6 +302,8 @@ export class LocationUpdatePage {
             // const stateAndDistrict = this.userLocations.filter(
             //     (loc: any) => loc.type === locationType.state || loc.type === locationType.district
             // );
+            const { firstName, gender, dob, profileCompleted } = this.profileData ?? {};
+            const profileCompletedPercentage = (firstName && gender && dob) ? 100 : (firstName || gender || dob) ? 60 : profileCompleted;
             const payload = {
                 params: {},
                 request: {
@@ -309,7 +311,7 @@ export class LocationUpdatePage {
                     // profileLocation: [...stateAndDistrict, ...Object.values(resolvedValues)]
                     profileLocation: [...Object.values(resolvedValues)],
                     profileUserTypes: subRoles,
-                    profileCompleted: this.profileData?.profileCompleted
+                    profileCompleted: profileCompletedPercentage
                 }
             }
             const updatedProfileData = { ...resolvedValues, [locationType.subPersona] : subRoles };
