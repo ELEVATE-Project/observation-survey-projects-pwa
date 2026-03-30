19efeadee7ba926a9e65cd34e854b66f9c45cd88
farhanp1502 <farhanpasha1502@gmail.com>
2026-03-30 11:21:17 +0530

    updated the angular version till 20

--- src/app/services/interceptor/api.interceptor.spec.ts
+++ src/app/services/interceptor/api.interceptor.spec.ts
@@ -1,11 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpInterceptorFn } from '@angular/common/http';
 
-import { apiInterceptor } from './api.interceptor';
 
 describe('apiInterceptor', () => {
   const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => apiInterceptor(req, next));
+    TestBed.runInInjectionContext(() => interceptor(req, next));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
